1eb582c9c71d2b93854e4f9c998a19736630da70
Tam Ireland <tamireland@hotmail.com>
2019-08-31 21:51:46 +0100

    Add project files.

---
+++ SereneApp.Web/Imports/ServerTypings/Northwind.CustomerRepresentativesRow.ts
@@ -0,0 +1,18 @@
+﻿namespace SereneApp.Northwind {
+    export interface CustomerRepresentativesRow {
+        RepresentativeId?: number;
+        CustomerId?: number;
+        EmployeeId?: number;
+    }
+
+    export namespace CustomerRepresentativesRow {
+        export const idProperty = 'RepresentativeId';
+        export const localTextPrefix = 'Northwind.CustomerRepresentatives';
+
+        export declare const enum Fields {
+            RepresentativeId = "RepresentativeId",
+            CustomerId = "CustomerId",
+            EmployeeId = "EmployeeId"
+        }
+    }
+}
